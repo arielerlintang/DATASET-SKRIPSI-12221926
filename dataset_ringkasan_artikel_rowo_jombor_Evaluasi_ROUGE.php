fdc609d747391dd54e74f6240c063503469c039a
Ariel Erlintang <115611813+arielerlintang@users.noreply.github.com>
2026-08-14 14:43:53 +0700

    Create dataset_ringkasan_artikel_rowo_jombor_Evaluasi_ROUGE.php

---
+++ dataset_ringkasan_artikel_rowo_jombor_Evaluasi_ROUGE.php
@@ -0,0 +1,11 @@
+Rowo Jombor adalah waduk buatan di Klaten, Jawa Tengah, yang menawarkan pemandangan danau luas berlatar perbukitan hijau.
+
+Suasananya yang asri bikin tempat ini cocok banget buat santai, foto-foto, atau sekadar menikmati vibes pedesaan.
+
+Daya tarik utamanya bukan cuma pemandangan, tapi juga kulineran di warung apung.
+
+Di sini, kita bisa makan berbagai olahan ikan air tawar langsung di atas air, pas banget buat tempat kumpul keluarga.
+
+Dengan potensi alamnya yang besar dan perbaikan fasilitas yang terus berjalan, Rowo Jombor berpeluang jadi wisata unggulan Klaten.
+
+Pengelolaan yang tepat nantinya gak cuma bakal manjain wisatawan, tapi juga bisa muter roda ekonomi warga lokal di sekitarnya.
